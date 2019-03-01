ed09b65a95479cfe42af43ce5ee34149964fa658
KylaPrograms <32083631+KylaPrograms@users.noreply.github.com>
2019-03-01 14:49:56 -0800

    Apparently, forgot to save before last push

--- TeamTeapot_FinalGame/public_html/src/MyGame/Objects/Hero.js
+++ TeamTeapot_FinalGame/public_html/src/MyGame/Objects/Hero.js
@@ -33,34 +33,18 @@ Hero.prototype.update = function()
 {
     GameObject.prototype.update.call(this);
     
-    var rb = this.getRigidBody();
-    var forward = this.getCurrentFrontDir();
-    
     if(gEngine.Input.isKeyPressed(gEngine.Input.keys.W))
     {
-        forward = [forward[0], forward[1] + Math.sin(90)];
-        rb.incVelocity(0, Math.sin(forward[1]) * 0.1);
     }
     if(gEngine.Input.isKeyPressed(gEngine.Input.keys.S))
     {
-        forward = [forward[0], forward[1] + Math.sin(270)];
-        rb.incVelocity(0, -Math.sin(forward[1]) * 0.1);
     }
     
     if(gEngine.Input.isKeyPressed(gEngine.Input.keys.A))
     {
-        forward = [forward[0] + Math.cos(90), forward[1]];
-        rb.incVelocity(-Math.cos(forward[0]) * 0.1, 0);
     }
     if(gEngine.Input.isKeyPressed(gEngine.Input.keys.D))
     {
-        forward = [forward[0] - Math.cos(90), forward[1]];
-        rb.incVelocity(Math.cos(forward[0]) * 0.1, 0);
     }
     
-    this.setCurrentFrontDir(forward[0], forward[1]);
-    var f = this.getCurrentFrontDir();
-    var rot = Math.atan2(f[0], f[1]);
-    console.log(f);
-    this.getXform().setRotationInRad(rot);
 };
