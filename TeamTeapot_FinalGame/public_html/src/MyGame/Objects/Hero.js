7ca26612d096eb9ae933e6afec4a8b37189fa6d1
KylaPrograms <32083631+KylaPrograms@users.noreply.github.com>
2019-03-01 16:52:49 -0800

    Ship movement?

--- TeamTeapot_FinalGame/public_html/src/MyGame/Objects/Hero.js
+++ TeamTeapot_FinalGame/public_html/src/MyGame/Objects/Hero.js
@@ -22,10 +22,7 @@ function Hero(spriteTexture)
     
     GameObject.call(this, this.mShip);
     
-    var r = new RigidRectangle(this.getXform(), 4, 8);
-    this.setRigidBody(r);
-    r.setMass(10);
-    r.setVelocity(0, 0);
+    this.mSpeed = 0;
 }
 gEngine.Core.inheritPrototype(Hero, GameObject);
 
@@ -33,18 +30,31 @@ Hero.prototype.update = function()
 {
     GameObject.prototype.update.call(this);
     
+    var dir = this.getCurrentFrontDir();
+    var pos = this.getXform().getPosition();
+    
     if(gEngine.Input.isKeyPressed(gEngine.Input.keys.W))
     {
+        this.mSpeed += 0.01;
     }
     if(gEngine.Input.isKeyPressed(gEngine.Input.keys.S))
     {
+        this.mSpeed -= 0.01;
+        if(this.mSpeed < 0)
+        {
+            this.mSpeed = 0;
+        }
     }
     
     if(gEngine.Input.isKeyPressed(gEngine.Input.keys.A))
     {
+        vec2.rotate(dir, dir, 0.02);
     }
     if(gEngine.Input.isKeyPressed(gEngine.Input.keys.D))
     {
+        vec2.rotate(dir, dir, -0.02);
     }
     
+    vec2.scaleAndAdd(pos, pos, dir, this.mSpeed);
+    this.getXform().setRotationInRad(Math.atan2(dir[0], -dir[1]));
 };
