e7d06ac55a5910ed1de7d9b25b8e8b5d90f3440d
jtduong22 <jtduong22@gmail.com>
2019-03-11 14:54:15 -0700

    Wake now trails ship

--- TeamTeapot_FinalGame/public_html/src/MyGame/Objects/WakeSet.js
+++ TeamTeapot_FinalGame/public_html/src/MyGame/Objects/WakeSet.js
@@ -32,25 +32,55 @@ WakeSet.prototype.update = function()
 WakeSet.prototype.createWakeFromShip = function(ship, sprite, size, speed)
 {
     var xform = ship.getXform();
+    
+    ///////////////
+    // LEFT WAKE //
+    ///////////////
+    
     var xPos = xform.getPosition()[0]-(xform.getSize()[0]/2);
     var yPos = xform.getPosition()[1]-(xform.getSize()[1]/2);
-    var theta = Math.atan2(ship.getCurrentFrontDir()[1], ship.getCurrentFrontDir()[0]);
-    theta -= 45;
+    
+    // Get vector
+    var newPos = [xPos - xform.getPosition()[0], yPos - xform.getPosition()[1]];
+    
+    // Get current rotation of ship
+    var theta = xform.getRotationInRad() - Math.PI;
+    
+    // Rotate to correct spot
+    vec2.rotateWRT(newPos, newPos, theta , [0,0]);
+
+    // move vector to behind ship
+    vec2.add(newPos, newPos, xform.getPosition());
+    theta += Math.PI / 4;
     var forward = [-Math.cos(theta), -Math.sin(theta)];
     
-    var leftWake = new Wake(sprite, [xPos, yPos], size, forward, speed);
-    leftWake.getXform().setRotationInRad(-forward[0], forward[1]);
+    var leftWake = new Wake(sprite, newPos, size, forward, speed);
+    leftWake.getXform().setRotationInRad(theta);
     
     this.addToSet(leftWake);
     
+    ////////////////
+    // RIGHT WAKE //
+    ////////////////
+    
     xPos = xform.getPosition()[0]+(xform.getSize()[0]/2);
     yPos = xform.getPosition()[1]-(xform.getSize()[1]/2);
-    theta = Math.atan2(ship.getCurrentFrontDir()[1], ship.getCurrentFrontDir()[0]);
-    theta += 45;
+    
+    // Get vector
+    newPos = [xPos - xform.getPosition()[0], yPos - xform.getPosition()[1]];
+    theta = xform.getRotationInRad() - Math.PI;
+    
+    // Rotate to current spot
+    vec2.rotateWRT(newPos, newPos, theta, [0,0]);
+    
+    // move vector to behind ship
+    vec2.add(newPos, newPos, xform.getPosition());
+    
+    theta -= Math.PI / 4;
     forward = [-Math.cos(theta), -Math.sin(theta)];
     
-    var rightWake = new Wake(sprite, [xPos, yPos], size, forward, speed);
-    rightWake.getXform().setRotationInRad(-forward[0], forward[1]);
+    var rightWake = new Wake(sprite, newPos, size, forward, speed);
+    rightWake.getXform().setRotationInRad(theta);
     
     this.addToSet(rightWake);
 };
